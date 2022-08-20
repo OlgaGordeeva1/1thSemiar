96ef63db932fbef540bac3938ca457b469055eb2
OlgaGord <gordeewa06@gmail.com>
2022-08-20 16:30:58 +0500

    Решена задача Ex003 с конвертацией

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -4,33 +4,33 @@
 
 
 Console.WriteLine("Введите число N: ");
-string N = Console.ReadLine()!;
+int N = Convert.ToInt32(Console.ReadLine());
 
-if (N == "1")
+if (N == 1)
 {    Console.WriteLine("Понедельник");}
 else
 {
-    if (N == "2")
+    if (N == 2)
     {Console.WriteLine("Вторник");}
     else 
     {
-        if (N == "3")
+        if (N == 3)
         {Console.WriteLine("Среда");}
         else 
         {
-            if (N == "4")
+            if (N == 4)
             {Console.WriteLine("Четверг");}
             else
             {
-                    if (N == "5")
+                    if (N == 5)
                 {Console.WriteLine("Пятница");}
                 else
                 {
-                    if (N == "6")
+                    if (N == 6)
                     {Console.WriteLine("Суббота");}
                     else
                     {
-                        if (N == "7")
+                        if (N == 7)
                         {Console.WriteLine("Воскресенье");}
                         else
                         {Console.WriteLine("Ошибка");}
