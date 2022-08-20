9f59ae9566d707317098f0d05012db9e678bb7b0
OlgaGord <gordeewa06@gmail.com>
2022-08-20 16:25:09 +0500

    Решена задача Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+
+Console.WriteLine("Введите число N: ");
+string N = Console.ReadLine()!;
+
+if (N == "1")
+{    Console.WriteLine("Понедельник");}
+else
+{
+    if (N == "2")
+    {Console.WriteLine("Вторник");}
+    else 
+    {
+        if (N == "3")
+        {Console.WriteLine("Среда");}
+        else 
+        {
+            if (N == "4")
+            {Console.WriteLine("Четверг");}
+            else
+            {
+                    if (N == "5")
+                {Console.WriteLine("Пятница");}
+                else
+                {
+                    if (N == "6")
+                    {Console.WriteLine("Суббота");}
+                    else
+                    {
+                        if (N == "7")
+                        {Console.WriteLine("Воскресенье");}
+                        else
+                        {Console.WriteLine("Ошибка");}
+                    }
+            
+            }
+
+            }
+            }
+    }
+}
